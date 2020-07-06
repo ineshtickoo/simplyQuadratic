88a13b5dbb59827f8eb2be4a611da52db5c71f52
Inesh Tickoo <42814528+ineshtickoo@users.noreply.github.com>
2020-07-06 22:00:51 +0530

    Divided the whole equation by coefficient of x^2
    Did this because when x^2's coefficient, a is more than 1, the program didn't display results correctly.

--- PoShenLohQuadratic.py
+++ PoShenLohQuadratic.py
@@ -2,23 +2,24 @@
 
 import math
 
-print("quadratic equations are of the form ax^2 + bx + c")
+print("Quadratic Equations are of the form f(x) = ax^2 + bx + c")
+print("Input the desired values of a, b and c for f(x) = 0")
 a = eval(input("a: "))
 b = eval(input("b: "))
 c = eval(input("c: "))
 
-u = 0
-h = b/2
-
+#declaring the half-value of b
+#restructuring the equation by dividing the other coefficients by the value of a
+h = (b/a)/2 
+u = math.sqrt(h**2 - c/a)
 #(h-u)(h+u)=c
-#h^2-u^2 = c
+#h^2-u^2 = c 
 #u^2=h^2-c
 #u=root(h^2-c)
 
-u = math.sqrt(h**2 - c)
-
-x1 = -1*(h-u)
-x2 = -1*(h+u)
+#solved values of x for f(x)
+x1 = (h-u) 
+x2 = (h+u) 
 
 
 print("The roots for this equation are: ", x1," and ",x2)
