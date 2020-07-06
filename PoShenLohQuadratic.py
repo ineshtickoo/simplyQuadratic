be49ae84aba60ad8f29206e2332df73bd715fac6
Inesh Tickoo <42814528+ineshtickoo@users.noreply.github.com>
2020-07-06 22:34:04 +0530

    added conditionals, used cmath for complex roots

--- PoShenLohQuadratic.py
+++ PoShenLohQuadratic.py
@@ -1,6 +1,8 @@
 #quadratic calculator
 
+import cmath
 import math
+from fractions import Fraction as frac
 
 print("Quadratic Equations are of the form f(x) = ax^2 + bx + c")
 print("Input the desired values of a, b and c for f(x) = 0")
@@ -11,15 +13,21 @@ c = eval(input("c: "))
 #declaring the half-value of b
 #restructuring the equation by dividing the other coefficients by the value of a
 h = (b/a)/2 
-u = math.sqrt(h**2 - c/a)
+usq = h**2 - c/a #u^2
 #(h-u)(h+u)=c
 #h^2-u^2 = c 
 #u^2=h^2-c
 #u=root(h^2-c)
 
 #solved values of x for f(x)
-x1 = (h-u) 
-x2 = (h+u) 
+if (h**2-c/a)<0:                #for unreal roots
+    u = cmath.sqrt(usq)
+    x1 = -1*(h-u)
+    x2 = -1*(h+u)
+else:                           #for real roots
+    u = math.sqrt(usq)
+    x1 = frac(-1*(h-u))
+    x2 = frac(-1*(h+u))    
 
 
 print("The roots for this equation are: ", x1," and ",x2)
